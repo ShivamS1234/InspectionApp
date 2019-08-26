0721448735e85ff963ee25b7fdba667a7aec0afc
ShivamS1234 <shivammcu@gmail.com>
2019-08-26 21:04:59 +0530

    api integration in binding picker field.

--- InspectionApp/InspectionApp/Helpers/InitData.cs
+++ InspectionApp/InspectionApp/Helpers/InitData.cs
@@ -9,83 +9,172 @@ using Inspection.Resouces.Entites;
 
 namespace InspectionApp.Helpers
 {
-    public static class InitData
+  public static class InitData
+  {
+    static int CompanyId;
+    static WebServiceManager webServiceManager = new WebServiceManager();
+    public static List<WarehouseDetails> WarehouseList
     {
-        static int CompanyName;
-        static WebServiceManager webServiceManager = new WebServiceManager();
-        public static List<WarehouseDetails> WarehouseList
-        {
-            get;
-            set;
-        }
-        public static List<ClientDetails> ClientList
-        {
-            get;
-            set;
-        }
-        public static List<UserDetails> UserList
-        {
-            get;
-            set;
-        }
-        public static List<Company> CmpList
-        {
-            get;
-            set;
-        }
-        public static List<Brand> BrandList
-        {
-            get;
-            set;
-        }
-
-        public static async void GetAllInitData()
-        {
-            try
-            {
-                UserDialogs.Instance.ShowLoading("Loading...");
-                CompanyName = Convert.ToInt32(RememberMe.Get("CmpID"));
-                CompaniesRequestDTO companiesRequestDTO = new CompaniesRequestDTO()
-                {
-                    IsActive = true,
-                };
-                var result = await webServiceManager.GetCompaniesbyAppIdAsync(companiesRequestDTO).ConfigureAwait(true);
-                if (result.IsSuccess)
-                {
-                    if (result.Data.Companies.Count > 0)
-                    {
-                        CmpList = new List<Company>();
-                        foreach (var data in result.Data.Companies)
-                        {
-                            CmpList.Add(data);
-                        }
-                    }
-                }
-                CommonRequestDTO BrandRequestDTO = new CommonRequestDTO()
-                {
-                    CompanyId = CompanyName
-                };
-                var resultBrand = await webServiceManager.GetBrandsbyCmpIdAsync(BrandRequestDTO).ConfigureAwait(true);
-                if (resultBrand.IsSuccess)
-                {
-                    if (resultBrand.Data.Brand.Count > 0)
-                    {
-                        BrandList = new List<Brand>();
-                        foreach (var data in resultBrand.Data.Brand)
-                        {
-                            BrandList.Add(data);
-                        }
-                    }
-                }
-                UserDialogs.Instance.HideLoading();
-            }
-            catch (Exception ex)
-            {
-                UserDialogs.Instance.HideLoading();
-                Console.WriteLine(ex.Message);
-                throw ex;
-            }
-        }
-
+      get;
+      set;
     }
+    public static List<ClientDetails> ClientList
+    {
+      get;
+      set;
+    }
+    public static List<UserDetails> UserList
+    {
+      get;
+      set;
+    }
+    public static List<Company> CmpList
+    {
+      get;
+      set;
+    }
+    public static List<Brand> BrandList
+    {
+      get;
+      set;
+    }
+    public static List<CountryofOrigin> CountryofOriginList
+    {
+      get;
+      set;
+    }
+    public static List<OpeningApperence> OpeningApperenceList
+    {
+      get;
+      set;
+    }
+    public static List<PackageCondition> PackageConditionList
+    {
+      get;
+      set;
+    }
+    public static List<PalletCondition> PalletConditionList
+    {
+      get;
+      set;
+    }
+    public static List<Producer> ProducerList
+    {
+      get;
+      set;
+    }
+    public static List<Product> ProductList
+    {
+      get;
+      set;
+    }
+    public static List<SizeTb> SizeTbList
+    {
+      get;
+      set;
+    }
+    public static List<Variety> VarietyList
+    {
+      get;
+      set;
+    }
+
+
+    public static async void GetAllInitData()
+    {
+      try
+      {
+        UserDialogs.Instance.ShowLoading("Loading...");
+        CompanyId = Convert.ToInt32(RememberMe.Get("CmpID"));
+        CommonRequestDTO commonRequestDTO = new CommonRequestDTO()
+        {
+          CompanyId = CompanyId
+        };
+        var result = await webServiceManager.GetAllDatabyCmpIdAsync(commonRequestDTO).ConfigureAwait(true);
+        if (result.IsSuccess)
+        {
+          if (result.Data.Brand.Count > 0)
+          {
+            BrandList = new List<Brand>();
+            foreach (var data in result.Data.Brand)
+            {
+              BrandList.Add(data);
+            }
+          }
+          if (result.Data.CountryofOrigin.Count > 0)
+          {
+            CountryofOriginList = new List<CountryofOrigin>();
+            foreach (var data in result.Data.CountryofOrigin)
+            {
+              CountryofOriginList.Add(data);
+            }
+          }
+          if (result.Data.OpeningApperence.Count > 0)
+          {
+            OpeningApperenceList = new List<OpeningApperence>();
+            foreach (var data in result.Data.OpeningApperence)
+            {
+              OpeningApperenceList.Add(data);
+            }
+          }
+          if (result.Data.PackageCondition.Count > 0)
+          {
+            PackageConditionList = new List<PackageCondition>();
+            foreach (var data in result.Data.PackageCondition)
+            {
+              PackageConditionList.Add(data);
+            }
+          }
+          if (result.Data.PackageCondition.Count > 0)
+          {
+            PalletConditionList = new List<PalletCondition>();
+            foreach (var data in result.Data.PalletCondition)
+            {
+              PalletConditionList.Add(data);
+            }
+          }
+          if (result.Data.PackageCondition.Count > 0)
+          {
+            ProducerList = new List<Producer>();
+            foreach (var data in result.Data.Producer)
+            {
+              ProducerList.Add(data);
+            }
+          }
+          if (result.Data.PackageCondition.Count > 0)
+          {
+            ProductList = new List<Product>();
+            foreach (var data in result.Data.Product)
+            {
+              ProductList.Add(data);
+            }
+          }
+          if (result.Data.PackageCondition.Count > 0)
+          {
+            SizeTbList = new List<SizeTb>();
+            foreach (var data in result.Data.SizeTb)
+            {
+              SizeTbList.Add(data);
+            }
+          }
+          if (result.Data.Variety.Count > 0)
+          {
+            VarietyList = new List<Variety>();
+            foreach (var data in result.Data.Variety)
+            {
+              VarietyList.Add(data);
+            }
+          }
+        }
+        UserDialogs.Instance.HideLoading();
+      }
+      catch (Exception ex)
+      {
+        UserDialogs.Instance.HideLoading();
+        Console.WriteLine(ex.Message);
+        throw ex;
+      }
+    }
+
+  }
 }
--- InspectionApp/InspectionApp/WebServices/WebServiceManager.cs
+++ InspectionApp/InspectionApp/WebServices/WebServiceManager.cs
@@ -10,182 +10,191 @@ using Newtonsoft.Json;
 
 namespace InspectionApp.WebServices
 {
-    public class WebServiceManager : ServiceConfigrations
+  public class WebServiceManager : ServiceConfigrations
+  {
+    public IWebService webService;
+    public WebServiceManager()
     {
-        public IWebService webService;
-        public WebServiceManager()
-        {
-            webService = new WebService();
-        }
-
-        public async Task<WebServiceResult<CompaniesResponseDTO>> GetCompaniesbyAppIdAsync(CompaniesRequestDTO companiesRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(companiesRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.GetAsync<CompaniesResponseDTO>("Company/GetAllCompanies", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<CommonResponseDTO>> GetBrandsbyCmpIdAsync(CommonRequestDTO companiesRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(companiesRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<CommonResponseDTO>("Brands/GetAllBrands", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<UsersResponseDTO>> RegistrationAsync(UsersRequestDTO companiesRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(companiesRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<UsersResponseDTO>("User/InsertUpdateUser", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<InspectionHeadersResponseDTO>> RegistrationInspectionHeaderAsync(InspectionHeadersRequestDTO inspectionRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(inspectionRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<InspectionHeadersResponseDTO>("InspectionHeaders/InsertInspectionHeaders", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<InspectionDetailsResponseDTO>> RegistrationInspectionDetailsAsync(InspectionDetailsRequestDTO inspectionRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(inspectionRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<InspectionDetailsResponseDTO>("InspectionDetails/InsertInspectionDetails", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<UsersResponseDTO>> LoginAsync(UsersRequestDTO userRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(userRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<UsersResponseDTO>("User/LoginByUserNamePassword", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<UsersResponseDTO>> userTbsRequestDTOAsync(UsersRequestDTO userRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(userRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<UsersResponseDTO>("User/GetUserListByUserIdAppIdCompIdRoleId", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<SendMailResponseDTO>> ProcessAuthEmail(SendMailRequestDTO sendMailRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(sendMailRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<SendMailResponseDTO>("Email/ProcessAuthEmail", utfString);
-            return result;
-        }
-
-        #region status api
-        public async Task<WebServiceResult<StatusResponseDTO>> GetStatusListByAppIdCompIdAsync(StatusRequestDTO statusRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(statusRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<StatusResponseDTO>("Status/GetStatusListByAppIdCompId", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<StatusResponseDTO>> InsertUpdateStatusAsync(StatusRequestDTO statusRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(statusRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<StatusResponseDTO>("Status/InsertUpdateStatus", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<StatusResponseDTO>> DeleteStatusbyStatusId(StatusRequestDTO freightTbsRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(freightTbsRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<StatusResponseDTO>("Status/DeleteStatusbyStatusId", utfString);
-            return result;
-        }
-        #endregion
-        #region warehouse
-        public async Task<WebServiceResult<WarehousesResponseDTO>> GetWarehousesListByAppIdCompIdAsync(WarehousesRequestDTO warehousesRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(warehousesRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<WarehousesResponseDTO>("Warehouse/GetWarehousesListByAppIdCompId", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<WarehousesResponseDTO>> InsertUpdateWharehosueAsync(WarehousesRequestDTO statusRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(statusRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<WarehousesResponseDTO>("Warehouse/InsertUpdateWarehouse", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<WarehousesResponseDTO>> DeleteWharehousebyStatusId(WarehousesRequestDTO freightTbsRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(freightTbsRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<WarehousesResponseDTO>("Warehouse/DeleteWarehousebyId", utfString);
-            return result;
-        }
-        #endregion
-        #region Inspection List
-        public async Task<WebServiceResult<InspectionHeadersResponseDTO>> GetHeaderbyID(InspectionHeadersRequestDTO warehousesRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(warehousesRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<InspectionHeadersResponseDTO>("InspectionHeaders/GetInspectionHeaderById", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<InspectionDetailsResponseDTO>> GetHeaderDetailsbyID(InspectionDetailsRequestDTO warehousesRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(warehousesRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<InspectionDetailsResponseDTO>("InspectionDetails/GetInspectionDetailsById", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<InspectionHeadersResponseDTO>> DeleteInspectionById(InspectionHeadersRequestDTO InspectionRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(InspectionRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<InspectionHeadersResponseDTO>("Freight/DeleteInspectionTbFreightId", utfString);
-            return result;
-        }
-        public async Task<WebServiceResult<InspectionHeadersResponseDTO>> DeleteInspectionDetailsById(InspectionHeadersRequestDTO InspectionRequestDTO)
-        {
-            UTF8Encoding encoder = new UTF8Encoding();
-            byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(InspectionRequestDTO));
-            string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
-
-            var result = await webService.PostAsync<InspectionHeadersResponseDTO>("Freight/DeleteInspectionTbFreightId", utfString);
-            return result;
-        }
-        #endregion
+      webService = new WebService();
     }
+
+    public async Task<WebServiceResult<CompaniesResponseDTO>> GetCompaniesbyAppIdAsync(CompaniesRequestDTO companiesRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(companiesRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.GetAsync<CompaniesResponseDTO>("Company/GetAllCompanies", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<CommonResponseDTO>> GetAllDatabyCmpIdAsync(CommonRequestDTO companiesRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(companiesRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<CommonResponseDTO>("AllData/GetAllData", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<CommonResponseDTO>> GetBrandsbyCmpIdAsync(CommonRequestDTO companiesRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(companiesRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<CommonResponseDTO>("Brands/GetAllBrands", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<UsersResponseDTO>> RegistrationAsync(UsersRequestDTO companiesRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(companiesRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<UsersResponseDTO>("User/InsertUpdateUser", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<InspectionHeadersResponseDTO>> RegistrationInspectionHeaderAsync(InspectionHeadersRequestDTO inspectionRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(inspectionRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<InspectionHeadersResponseDTO>("InspectionHeaders/InsertInspectionHeaders", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<InspectionDetailsResponseDTO>> RegistrationInspectionDetailsAsync(InspectionDetailsRequestDTO inspectionRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(inspectionRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<InspectionDetailsResponseDTO>("InspectionDetails/InsertInspectionDetails", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<UsersResponseDTO>> LoginAsync(UsersRequestDTO userRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(userRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<UsersResponseDTO>("User/LoginByUserNamePassword", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<UsersResponseDTO>> userTbsRequestDTOAsync(UsersRequestDTO userRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(userRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<UsersResponseDTO>("User/GetUserListByUserIdAppIdCompIdRoleId", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<SendMailResponseDTO>> ProcessAuthEmail(SendMailRequestDTO sendMailRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(sendMailRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<SendMailResponseDTO>("Email/ProcessAuthEmail", utfString);
+      return result;
+    }
+
+    #region status api
+    public async Task<WebServiceResult<StatusResponseDTO>> GetStatusListByAppIdCompIdAsync(StatusRequestDTO statusRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(statusRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<StatusResponseDTO>("Status/GetStatusListByAppIdCompId", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<StatusResponseDTO>> InsertUpdateStatusAsync(StatusRequestDTO statusRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(statusRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<StatusResponseDTO>("Status/InsertUpdateStatus", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<StatusResponseDTO>> DeleteStatusbyStatusId(StatusRequestDTO freightTbsRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(freightTbsRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<StatusResponseDTO>("Status/DeleteStatusbyStatusId", utfString);
+      return result;
+    }
+    #endregion
+    #region warehouse
+    public async Task<WebServiceResult<WarehousesResponseDTO>> GetWarehousesListByAppIdCompIdAsync(WarehousesRequestDTO warehousesRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(warehousesRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<WarehousesResponseDTO>("Warehouse/GetWarehousesListByAppIdCompId", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<WarehousesResponseDTO>> InsertUpdateWharehosueAsync(WarehousesRequestDTO statusRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(statusRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<WarehousesResponseDTO>("Warehouse/InsertUpdateWarehouse", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<WarehousesResponseDTO>> DeleteWharehousebyStatusId(WarehousesRequestDTO freightTbsRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(freightTbsRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<WarehousesResponseDTO>("Warehouse/DeleteWarehousebyId", utfString);
+      return result;
+    }
+    #endregion
+    #region Inspection List
+    public async Task<WebServiceResult<InspectionHeadersResponseDTO>> GetHeaderbyID(InspectionHeadersRequestDTO warehousesRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(warehousesRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<InspectionHeadersResponseDTO>("InspectionHeaders/GetInspectionHeaderById", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<InspectionDetailsResponseDTO>> GetHeaderDetailsbyID(InspectionDetailsRequestDTO warehousesRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(warehousesRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<InspectionDetailsResponseDTO>("InspectionDetails/GetInspectionDetailsById", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<InspectionHeadersResponseDTO>> DeleteInspectionById(InspectionHeadersRequestDTO InspectionRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(InspectionRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<InspectionHeadersResponseDTO>("Freight/DeleteInspectionTbFreightId", utfString);
+      return result;
+    }
+    public async Task<WebServiceResult<InspectionHeadersResponseDTO>> DeleteInspectionDetailsById(InspectionHeadersRequestDTO InspectionRequestDTO)
+    {
+      UTF8Encoding encoder = new UTF8Encoding();
+      byte[] data = encoder.GetBytes(JsonConvert.SerializeObject(InspectionRequestDTO));
+      string utfString = Encoding.UTF8.GetString(data, 0, data.Length);
+
+      var result = await webService.PostAsync<InspectionHeadersResponseDTO>("Freight/DeleteInspectionTbFreightId", utfString);
+      return result;
+    }
+    #endregion
+  }
 }
