59cb71daed9c65bb5ea7b910d6d7d8b627b5b8e4
ShivamS1234 <shivammcu@gmail.com>
2019-10-22 17:32:32 +0530

    done both offline-online functionality.

--- InspectionApp/InspectionApp/View/DashboardPage.xaml.cs
+++ InspectionApp/InspectionApp/View/DashboardPage.xaml.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-
+using InspectionApp.ViewModel;
 using Xamarin.Forms;
 
 namespace InspectionApp.View
@@ -12,5 +12,12 @@ namespace InspectionApp.View
       InitializeComponent();
       NavigationPage.SetHasNavigationBar(this, false);
     }
+
+    //protected override void OnAppearing()
+    //{
+    //  base.OnAppearing();
+    //  var data = this.BindingContext as DashBoardViewModel;
+
+    //}
   }
 }
